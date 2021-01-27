c8ea33ddf0671a8435b6b5dbcd8a592dc60a06bb
Frank Tran <franktranvantu@gmail.com>
2021-01-27 23:19:19 +0700

    Update

---
+++ final/js/app.js
@@ -0,0 +1,10 @@
+// $('#flashMessage').hide();
+// $('#flashMessage').slideDown(1000);
+// $('#flashMessage').delay(2000);
+// $('#flashMessage').slideUp();
+
+$('#flashMessage')
+  .hide()
+  .slideDown(1000)
+  .delay(2000)
+  .slideUp();
