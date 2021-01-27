76561badac5157e7caa1102b837a624ad04a66fd
Frank Tran <franktranvantu@gmail.com>
2021-01-27 21:13:55 +0700

    Update

---
+++ final/js/app.js
@@ -0,0 +1,13 @@
+// JavaScript
+const box = document.querySelector('.box');
+box.style.display = 'none';
+// box.addEventListener('click', function () {
+//     alert('You clicked me!');
+// });
+
+// jQuery
+// jQuery('.box').hide();
+// $('.box').hide();
+// $('.box').click(function () {
+//     alert('You clicked me!');
+// });
