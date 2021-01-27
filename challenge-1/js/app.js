a10b2d6cf547fb648c2ac86e594a225a46f56d78
Frank Tran <franktranvantu@gmail.com>
2021-01-27 23:56:41 +0700

    Update

---
+++ challenge-1/js/app.js
@@ -0,0 +1,3 @@
+// Convert the plain JavaScript code in the app.js file to jQuery.
+// Assume the selected element has a class of profile-header, and use the jQuery method .hide().
+document.querySelector('.profile-header').style.display = 'none';
---
+++ solution-1/js/app.js
@@ -0,0 +1,3 @@
+// Convert the plain JavaScript code in the app.js file to jQuery.
+// Assume the selected element has a class of profile-header, and use the jQuery method .hide().
+$('.profile-header').hide();
