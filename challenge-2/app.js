39ebbba3d12a87e6e5b2ee3470e62978db80e92e
Frank Tran <franktranvantu@gmail.com>
2021-01-28 00:11:53 +0700

    Update

---
+++ challenge-2/app.js
@@ -0,0 +1,3 @@
+// You've just learned how to use the jQuery methods text() and html().
+// Use the appropriate jQuery method to set the contents of the $('.profile-text') element to "I am a web developer".
+$('.profile-text');
---
+++ solution-1/app.js
@@ -0,0 +1,4 @@
+// Take a look at the index.html file and examine the attributes on each element.
+// Use jQuery to select the paragraph tag by its class name.
+// You don't need to save your jQuery selection to a variable or call any methods on it yet.
+$('.profile-text');
