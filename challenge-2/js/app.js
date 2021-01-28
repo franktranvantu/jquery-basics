c61f6a8e49ba01c3a5d300531335e41a541fb2fd
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-28 09:24:23 +0700

    Update

--- challenge-2/js/app.js
+++ challenge-2/js/app.js
@@ -1,6 +1,3 @@
-// Convert the plain JavaScript code in the app.js file to jQuery.
-// Assume the selected element has a class of profile-header, and use the jQuery method .hide().
-$('.profile-header').hide();
-
 // Chain some animation effects to the $('.profile-header') element.
 // After a delay() of 2 seconds (2000 milliseconds), use the fadeIn() method to fade the header in.
+$('.profile-header').hide();
