9e184554d5035d66d1802772298af2a6d16b75eb
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-28 09:50:26 +0700

    Update

--- final/js/app.js
+++ final/js/app.js
@@ -1,11 +1,14 @@
-$('#flashMessage')
-  .hide()
-  .slideDown(1000)
-  .delay(2000)
-  .slideUp();
+$('#flashMessage').hide();
 
-const title = 'My first blog post';
-const content = 'This is my <strong>first</strong> post!';
+$('#previewButton').click(function () {
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
 
-$('#blogTitlePreview').text(title);
-$('#blogContentPreview').html(content);
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').html(content);
+
+  $('#flashMessage')
+    .slideDown(1000)
+    .delay(2000)
+    .slideUp();
+});
--- start/js/app.js
+++ start/js/app.js
@@ -3,3 +3,9 @@ $('#flashMessage')
   .slideDown(1000)
   .delay(2000)
   .slideUp();
+
+const title = 'My first blog post';
+const content = 'This is my <strong>first</strong> post!';
+
+$('#blogTitlePreview').text(title);
+$('#blogContentPreview').html(content);
