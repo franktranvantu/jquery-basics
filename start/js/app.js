de76f69dd938b8f8cc2618cbd115a224a09c427a
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-29 09:54:48 +0700

    Update

--- start/js/app.js
+++ start/js/app.js
@@ -1,4 +1,9 @@
 // Hide the spoiler text
+$('.spoiler span').hide();
 // When the button is clicked
-// Show the spoiler text
-// Hide the "Reveal Spoiler" button
+$('.spoiler button').click(function () {
+  // Show the spoiler text
+  $('.spoiler span').show();
+  // Hide the "Reveal Spoiler" button
+  $('.spoiler button').hide();
+});
