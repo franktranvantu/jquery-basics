0487daac77b0aad1ab2e0af38ebc9af013ab9ca1
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-29 13:15:28 +0700

    Update

--- start/js/app.js
+++
@@ -1,9 +0,0 @@
-// Hide the spoiler text
-$('.spoiler span').hide();
-// When the button is clicked
-$('.spoiler button').click(function () {
-  // Show the spoiler text
-  $('.spoiler span').show();
-  // Hide the "Reveal Spoiler" button
-  $('.spoiler button').hide();
-});
