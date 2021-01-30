565b386950357524fc5f38d22d6ece008f552387
Frank Tran <franktranvantu@gmail.com>
2021-01-30 13:29:48 +0700

    Update

---
+++ solution-1/app.js
@@ -0,0 +1,4 @@
+// Select the submit button by its class and save it to a variable called $submit.
+// Then use the appropriate jQuery method to disable the button by adding a disabled attribute to it.
+const $submit = $('.submit-btn');
+$submit.attr('disabled', true);
--- start/js/app.js
+++
@@ -1,6 +0,0 @@
-const $odd = $('a:odd');
-const $secureLinks = $('a[href^="https://"]');
-const $pdfLinks = $('a[href$="pdf"]');
-
-$secureLinks.attr('target', '_blank');
-$pdfLinks.attr('download', true);
