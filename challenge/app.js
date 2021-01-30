ce9575c1b5b6f567688cbd7786c4b8de75cb9e4a
Frank Tran <franktranvantu@gmail.com>
2021-01-30 21:28:24 +0700

    Update

---
+++ challenge/app.js
@@ -0,0 +1,5 @@
+// Inside the jQuery each() method, use prepend() to add an '<input type="checkbox"/>' to the beginning of each list item.
+// Use the this keyword, so the callback function doesn't need any arguments.
+$('.favorite-things li').each(function() {
+  // Write code here
+});
