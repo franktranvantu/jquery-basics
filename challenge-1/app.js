74126cac16c40f3cc52be0e1336d9bf37aef5798
Frank Tran <franktranvantu@gmail.com>
2021-01-30 12:56:40 +0700

    Update

--- challenge-1/app.js
+++
@@ -1,2 +0,0 @@
-// Using jQuery, select all of the list items on the page.
-// Then, using the .eq() method, select Wade Christensen from the list of students.
--- solution-1/app.js
+++
@@ -1,3 +0,0 @@
-// Using jQuery, select all of the list items on the page.
-// Then, using the .eq() method, select Wade Christensen from the list of students.
-$('li').eq(2);
