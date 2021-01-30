bb79925c6173d3aeb2d95115c85e759b4b9021a3
Frank Tran <franktranvantu@gmail.com>
2021-01-30 12:10:38 +0700

    Update

---
+++ challenge-1/app.js
@@ -0,0 +1,2 @@
+// Using jQuery, select all of the list items on the page.
+// Then, using the .eq() method, select Wade Christensen from the list of students.
---
+++ solution-2/app.js
@@ -0,0 +1,3 @@
+// You've just learned about the next() and prev() methods.
+// Use one of them to traverse from Wade Christensen to James McAvoy in the list of students.
+$('li').eq(2).prev().prev();
