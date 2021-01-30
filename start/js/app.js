4278fd6ceafcd065409154b3a72c60e01c281872
Frank Tran <franktranvantu@gmail.com>
2021-01-30 20:46:57 +0700

    Update

--- start/js/app.js
+++ start/js/app.js
@@ -7,3 +7,9 @@ $pdfLinks.attr('download', true);
 
 $secureLinks.addClass('secure');
 $pdfLinks.addClass('pdf');
+
+// Check if checkbox has been checked
+// If zero checkboxes are checked
+  // Prevent download of document
+  // Alert the user
+// Else allow the download
