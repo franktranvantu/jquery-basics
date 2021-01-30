dfaf17362d9be1116c8a05524ff1ca538e0f540a
Frank Tran <franktranvantu@gmail.com>
2021-01-30 13:00:23 +0700

    Update

---
+++ challenge-1/app.js
@@ -0,0 +1,2 @@
+// CSS is being used to hide three items on the index.html page (two <li> elements and a <div> element).
+// Use jQuery's :hidden pseudo selector and the show() method to display the hidden <li> elements, while leaving the <div> element hidden.
--- final/js/app.js
+++
@@ -1,8 +0,0 @@
-const $odd = $('a:odd');
-$odd.css({backgroundColor: 'tomato'});
-
-const $secureLinks = $('a[href^="https://"]');
-$secureLinks.css({color: 'green'});
-
-const $pdfLinks = $('a[href$="pdf"]');
-$pdfLinks.css({fontSize: '2rem'});
