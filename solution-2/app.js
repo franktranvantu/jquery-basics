6f263986d6498e0008a743cf2ba9727d564ceaee
Frank Tran <franktranvantu@gmail.com>
2021-02-02 21:42:07 +0700

    Update

--- solution-2/app.js
+++ solution-2/app.js
@@ -1,4 +1,4 @@
 // The submit button is now disabled, but it doesn’t look like it! Luckily, we have a class called disabled in our CSS that will style it for us.
 // Chain the appropriate jQuery method to add the class of disabled to the submit button.
 const $submit = $('.submit-btn');
-$submit.attr('disabled', true).addClass('disabled');
+$submit.addClass('disabled');
